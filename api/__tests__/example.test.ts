fad4fc84573d6e36e38d0a6116525b7a8be70d45
rebeccalindman <rebeccalindman@icloud.com>
2025-10-08 16:30:17 +0200

    test success

--- api/__tests__/example.test.ts
+++ api/__tests__/example.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 
 describe('Sample test', () => {
   test('should pass', () => {
-    expect(1 + 1).toBe(3);
+    expect(1 + 1).toBe(2);
   });
 });
 
